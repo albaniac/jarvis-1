7aabb406a91ffb3abfa56c1aa7cc33fd72bcf22f
pessonnier <loic.pessonnier@gmail.com>
2017-01-13 10:32:13 +0100

    politesse

--- jarvis.sh
+++ jarvis.sh
@@ -8,6 +8,7 @@ show_help () { cat <<EOF
 
     Usage: ${0##*/} [-$flags]
 
+    Alu,
     Jarvis.sh is a lightweight configurable multi-lang jarvis-like bot
     Meant for home automation running on slow computer (ex: Raspberry Pi)
     It installs automatically speech recognition & synthesis engines of your choice
